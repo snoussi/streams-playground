3935ad265c1c690cb4ecc937386920b413bb6c57
snoussi <rachid+github@redhat.com>
2023-12-06 13:35:56 +0100

    split/branch logic

--- src/main/java/com/redhat/wallet/PaymentsStream.java
+++ src/main/java/com/redhat/wallet/PaymentsStream.java
@@ -5,11 +5,13 @@ import javax.enterprise.inject.Produces;
 
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueStore;
+
+import java.util.Map;
 
 
 @ApplicationScoped
@@ -28,11 +30,35 @@ public class PaymentsStream {
 
         stream.foreach((key, amount) -> System.out.println("Received payment: " + amount));
 
-        stream
-            .filter((key, amount) -> amount > 1000)
-            .to("large-payments", Produced.with(keySerde, valueSerde));
+
+        KStream<String, Integer> filteredStream  = stream.filter((key, amount) -> amount > 0);
+
+        Map<String, KStream<String, Integer>> branches = filteredStream.split(Named.as("payments-"))
+                .branch(
+                        (key, amount)-> (amount >= 1000 && amount <= 10000),
+                        Branched.withConsumer(ks -> ks.to("large-payments", Produced.with(keySerde, valueSerde)),"large"))
+                .branch(
+                        (key, amount)-> amount > 10000,
+                        Branched.withConsumer(ks -> ks.to("very-large-payments", Produced.with(keySerde, valueSerde)),"very-large"))
+                .defaultBranch(
+                        Branched.as("small"));
+
+        branches.get("payments-small").to("small-payments", Produced.with(keySerde, valueSerde));
+
+//        KTable<String, Integer> lastLargePaymentKTable = builder.table(
+//                "large-payments",
+//                Consumed.with(keySerde,valueSerde)
+//        );
+//
+//        lastLargePaymentKTable.toStream().peek((key, amount) -> System.out.println("last large payment: " + amount + " by "+ key));
+//
+//        GlobalKTable<String, Integer> lastVeryLargePaymentGlobalKTable = builder.globalTable(
+//                "very-large-payments",
+//                Consumed.with(keySerde,valueSerde)
+//        );
 
         return builder.build();
+
     }
 
 }
